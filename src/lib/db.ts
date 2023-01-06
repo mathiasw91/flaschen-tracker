5d4b7d824b3b76a074305573294cc2debcf2323e
mathiasw91 <widera.mathias@gmail.com>
2023-01-06 10:21:32 +0100

    log environment variable

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,6 @@
 import { MongoClient } from "mongodb"
 
+console.log('ENV', process.env.MONGODB_URI);
 const uri = process.env.MONGODB_URI as string;
 const client = new MongoClient(uri);
 await client.connect();
